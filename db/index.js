dabd4abfe3d6b58770c478a87f0c8718064d8059
beniciolevy <blevy@udesa.edu.ar>
2025-04-13 17:56:16 -0300

    Avanzamos con el punto 2 con los arrays y linkearlos

--- db/index.js
+++ db/index.js
@@ -1,28 +1,204 @@
 
 //Base de datos de los productos
-let producto = {
-    imagen: '/images/products/img-cafetera-moulinex.jpg',
-    nombre: 'Cafetera Moulinex',
-    descripcion: 'Es una cafetera fácil de usar y express.',
+let productos = [
+    {
+    id: 1,
+    imagen: "/images/products/img-cafetera-moulinex.jpg",
+    nombre: "Cafetera Moulinex",
+    descripcion: "Es una cafetera fácil de usar y express.",
     comentarios: [
         {
-            nombre: 'Miguel',
-            texto: 'Muy buena la cafetera, me encantaron los colores.',
-            imagen: 'foto-miguel.png'
+            nombre: "Miguel",
+            texto: "Muy buena la cafetera, me encantaron los colores.",
+            imagen: "foto-miguel.png"
         },
         {
-            nombre: 'Pato',
-            texto: '¡Precio excesivo!',
-            imagen: 'foto-pato.png'
+            nombre: "Pato",
+            texto: "Precio excesivo!",
+            imagen: "foto-pato.png"
         }
     ]
-};
+
+},
+
+{   id: 2,
+    imagen: "/images/products/img-macbook-pro-2019.jpg",
+    nombre: "Macbook pro 2019",
+    descripcion: "Computadora para programar, navegar, eficiente.",
+    comentarios: [
+        {
+            nombre: "Miguel",
+            texto: "Muy buena compu, muy rapida",
+            imagen: "/images/users/default-image.png"
+        },
+        {
+            nombre: "Pato",
+            texto: "Bastante cara",
+            imagen: "/images/users/default-image.png"
+        }
+    ]
+
+},
+{   id: 3,
+    imagen: "/images/products/img-samsung-galaxy-s10.jpg",
+    nombre: "Samsung Galaxy S10",
+    descripcion: "Telefono de ultima generacion con AI.",
+    comentarios: [
+        {
+            nombre: "Miguel",
+            texto: "Me encanto! Lo uso todos los dias",
+            imagen: "/images/users/default-image.png"
+        },
+        {
+            nombre: "Pato",
+            texto: "Muy bueno!",
+            imagen:"/images/users/default-image.png"
+        }
+    ]
+
+},
+{   id: 4,
+    imagen: "/images/products/img-tv-samsung-smart.jpg",
+    nombre: "Cafetera Moulinex",
+    descripcion: "Television Smart con resolucion 4k.",
+    comentarios: [
+        {
+            nombre: "Miguel",
+            texto: "Funciona muy bien.",
+            imagen: "/images/users/default-image.png"
+        },
+        {
+            nombre: "Pato",
+            texto: "Bastante cara!",
+            imagen: "/images/users/default-image.png"
+        }
+    ]
+
+},
+
+{   id: 5,
+    imagen: "/images/products/img-cafetera-moulinex.jpg",
+    nombre: "Cafetera Moulinex",
+    descripcion: "Es una cafetera fácil de usar y express.",
+    comentarios: [
+        {
+            nombre: "Miguel",
+            texto: "Muy buena la cafetera, me encantaron los colores.",
+            imagen: "foto-miguel.png"
+        },
+        {
+            nombre: "Pato",
+            texto: "Precio excesivo!",
+            imagen: "foto-pato.png"
+        }
+    ]
+
+},
+
+{   id: 6,
+    imagen: "/images/products/img-macbook-pro-2019.jpg",
+    nombre: "Macbook pro 2019",
+    descripcion: "Computadora para programar, navegar, eficiente.",
+    comentarios: [
+        {
+            nombre: "Miguel",
+            texto: "Muy buena compu, muy rapida",
+            imagen: "/images/users/default-image.png"
+        },
+        {
+            nombre: "Pato",
+            texto: "Bastante cara",
+            imagen: "/images/users/default-image.png"
+        }
+    ]
+
+},
+{   id: 7,
+    imagen: "/images/products/img-samsung-galaxy-s10.jpg",
+    nombre: "Samsung Galaxy S10",
+    descripcion: "Telefono de ultima generacion con AI.",
+    comentarios: [
+        {
+            nombre: "Miguel",
+            texto: "Me encanto! Lo uso todos los dias",
+            imagen: "/images/users/default-image.png"
+        },
+        {
+            nombre: "Pato",
+            texto: "Muy bueno!",
+            imagen:"/images/users/default-image.png"
+        }
+    ]
+
+},
+{   id: 8,
+    imagen: "/images/products/img-tv-samsung-smart.jpg",
+    nombre: "Cafetera Moulinex",
+    descripcion: "Television Smart con resolucion 4k.",
+    comentarios: [
+        {
+            nombre: "Miguel",
+            texto: "Funciona muy bien.",
+            imagen: "/images/users/default-image.png"
+        },
+        {
+            nombre: "Pato",
+            texto: "Bastante cara!",
+            imagen: "/images/users/default-image.png"
+        }
+    ]
+
+},
+
+
+{   id: 9,
+    imagen: "/images/products/img-cafetera-moulinex.jpg",
+    nombre: "Cafetera Moulinex",
+    descripcion: "Es una cafetera fácil de usar y express.",
+    comentarios: [
+        {
+            nombre: "Miguel",
+            texto: "Muy buena la cafetera, me encantaron los colores.",
+            imagen: "foto-miguel.png"
+        },
+        {
+            nombre: "Pato",
+            texto: "Precio excesivo!",
+            imagen: "foto-pato.png"
+        }
+    ]
+
+},
+
+{   id: 10,
+    imagen: "/images/products/img-macbook-pro-2019.jpg",
+    nombre: "Macbook pro 2019",
+    descripcion: "Computadora para programar, navegar, eficiente.",
+    comentarios: [
+        {
+            nombre: "Miguel",
+            texto: "Muy buena compu, muy rapida",
+            imagen: "/images/users/default-image.png"
+        },
+        {
+            nombre: "Pato",
+            texto: "Bastante cara",
+            imagen: "/images/users/default-image.png"
+        }
+    ]
+
+},
+
+
+
+
+]
 
 //Base de datos del usuario
 let usuario = {
-    email: 'genarogonzales@gmail.com',
-    password: '1234',
-    fecha: '08/04/2025',
+    email: "genarogonzales@gmail.com",
+    password: "1234",
+    fecha: "08/04/2025",
     dni: 44123771,
     nombre: "Genaro",
     foto: "/images/users/foto-usuario.jpg"
@@ -31,7 +207,10 @@ let usuario = {
 
 
 
-module.exports = usuario;
+module.exports = {
+    usuario,
+    productos
+}
 
 
 
--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const productosController = require("../controller/productosController");
 
 router.get("/", productosController.product);
+
 router.get("/add", productosController.addProduct);
+
+router.get("/:id", productosController.productDetail)
 
 module.exports = router;
